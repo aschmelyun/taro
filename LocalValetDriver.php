0990fc7854c5b619e3917e9a3471c4e11ce65671
chandlercodes <ryan@chandler.codes>
2019-02-13 09:50:32 +0000

    Simplified driver to mutate URIs and not return a front controller

--- LocalValetDriver.php
+++ LocalValetDriver.php
@@ -3,6 +3,17 @@
 class LocalValetDriver extends ValetDriver
 {
     /**
+     * Mutates the incoming URI.
+     *
+     * @param string $uri
+     * @return void
+     */
+    public function mutateUri($uri)
+    {
+        return rtrim("/dist{$uri}/");
+    }
+
+    /**
      * Determine if the driver serves the request.
      *
      * @param  string  $sitePath
@@ -12,38 +23,6 @@ class LocalValetDriver extends ValetDriver
      */
     public function serves($sitePath, $siteName, $uri)
     {
-        if (file_exists($sitePath . '/dist/index.html')) {
-            return true;
-        }
-    }
-
-    /**
-     * Determine if the incoming request is for a static file.
-     *
-     * @param  string  $sitePath
-     * @param  string  $siteName
-     * @param  string  $uri
-     * @return string|false
-     */
-    public function isStaticFile($sitePath, $siteName, $uri)
-    {
-        if (file_exists($staticFilePath = $sitePath . '/dist/' . $uri)) {
-            return $staticFilePath;
-        }
-
-        return false;
-    }
-
-    /**
-     * Get the fully resolved path to the application's front controller.
-     *
-     * @param  string  $sitePath
-     * @param  string  $siteName
-     * @param  string  $uri
-     * @return string
-     */
-    public function frontControllerPath($sitePath, $siteName, $uri)
-    {
-        return $sitePath . '/dist/index.html';
+        return is_dir("{$sitePath}/dist/");
     }
 }
